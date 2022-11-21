d3da5402a7e65a0b7e4a0688e067c54e95c8943a
user03 <zaika0180@gmail.com>
2022-11-21 23:37:52 +0900

    next

--- app/src/main/java/com/example/spravochnicgrib/main_menu.kt
+++ app/src/main/java/com/example/spravochnicgrib/main_menu.kt
@@ -3,6 +3,8 @@ package com.example.spravochnicgrib
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 
 class main_menu : AppCompatActivity() {
@@ -10,6 +12,23 @@ class main_menu : AppCompatActivity() {
         setTheme(R.style.Theme_SpravochnicGrib)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        when(item.itemId) {
+            R.id.o_program -> {
+                val intent = Intent(this@main_menu, docuventation::class.java)
+                intent.addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                            Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun aidovito(view: View) {
